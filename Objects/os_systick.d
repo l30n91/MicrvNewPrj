46d300509f3ef33d23986e10355be5cfaf8d17ec
l30n491 <leonardo.buonaiuto91@gmail.com>
2026-02-26 16:22:41 +0100

    Event Recorder - USART2 configured

--- Objects/os_systick.d
+++ Objects/os_systick.d
@@ -1,8 +1,14 @@
 ./objects/os_systick.o: \
-  C:\Users\bl91\AppData\Local\Arm\Packs\ARM\CMSIS\6.3.0\CMSIS\RTOS2\Source\os_systick.c \
-  C:\Users\bl91\AppData\Local\Arm\Packs\ARM\CMSIS\6.3.0\CMSIS\RTOS2\Include\os_tick.h \
+  C:\Users\bl91\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\RTOS2\Source\os_systick.c \
+  C:\Users\bl91\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\RTOS2\Include\os_tick.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
   RTE\_Target_1\RTE_Components.h \
   C:\Users\bl91\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
   C:\Users\bl91\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f446xx.h \
-  C:\Users\bl91\AppData\Local\Arm\Packs\ARM\CMSIS\6.3.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\bl91\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\bl91\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\bl91\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\bl91\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\.\m-profile\cmsis_armclang_m.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Users\bl91\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\m-profile\armv7m_mpu.h \
   C:\Users\bl91\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include "EventRecorder.h"
 
 void GPIO_Init(void);
+void USART2_init(void);
+void USART2_WriteTest(void);
 
 //typedef void (*osThreadFunc_t) (void *argument);
 osThreadFunc_t Task1_p;   //un puntatore a funzione è un puntatore non è una funzione, quindi è
@@ -69,15 +71,19 @@ void createTask1(osThreadFunc_t Task)
 	
 	//SystemCoreClockUpdate();
 	GPIO_Init();
-	EventRecorderInitialize(EventRecordAll, 1U);
+	USART2_init();
+  USART2_WriteTest(); /* Check if USART2 Writes on the terminal*/
+  EventRecorderInitialize(EventRecordAll, 1U);
 	EventRecorderStart();
-	Task1_p=Task1; /*solo a scopo dimostrativo passo il puntatore, potrei passare direttamente la funzione Task1 ad osThreadNew*/
+	Task1_p = Task1; /*solo a scopo dimostrativo passo il puntatore, potrei passare direttamente la funzione Task1 ad osThreadNew*/
 	osKernelInitialize();
 	shared_queue = osMessageQueueNew(4, 8*sizeof(uint8_t), NULL);
 	createTask1(Task1_p);
 	createTask2(Task2);
 	osKernelStart();
-	for(;;) {}
+	for(;;) {
+  
+  }
 	
 }
 
@@ -90,4 +96,42 @@ void GPIO_Init(void)
   GPIOA->MODER |=  0x00000400;    /* set pin to output mode */
   GPIOA->MODER |=  (1U << (5 * 2));  // set PA5 come output	
 }
- 
+
+
+
+void USART2_init(void) {
+    // Clock GPIOA e USART2
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+    RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+
+    // PA2 = TX (AF7), PA3 = RX (AF7)
+    GPIOA->MODER &= ~((3U<<(2*2)) | (3U<<(2*3)));   // clear PA2, PA3
+    GPIOA->MODER |=  ((2U<<(2*2)) | (2U<<(2*3)));   // Alternate Function
+    GPIOA->AFR[0] &= ~((0xFU<<(4*2)) | (0xFU<<(4*3)));
+    GPIOA->AFR[0] |=  ((7U  <<(4*2)) | (7U  <<(4*3))); // AF7
+
+    // UART: 8N1, no flow control
+    USART2->CR1 = 0;
+    USART2->CR2 = 0;
+    USART2->CR3 = 0;
+    // Scegli il BRR giusto per il tuo PCLK1
+    USART2->BRR = 0x08B;  // 115200 @ PCLK1=16 MHz
+    //USART2->BRR = 0x16D;     // 115200 @ PCLK1=42 MHz
+    USART2->CR1 |= USART_CR1_TE | USART_CR1_RE;  // << abilita TX e RX
+    USART2->CR1 |= USART_CR1_UE;                 // abilita USART
+}
+
+
+void USART2_WriteTest(void) {
+char string_[]= "anna ";
+     uint8_t* p_string=(uint8_t*)string_;
+     
+     while(*p_string !=' ')
+     {
+       while(!(USART2->SR & USART_SR_TXE)); /* wait until TX is enabled*/
+       
+       USART2->DR= *p_string;
+       
+       p_string ++;
+     }
+}
